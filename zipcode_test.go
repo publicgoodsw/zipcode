607e9dff762b0ecba17dc593b06b5d7e2b01595f
Paul Smith <paulsmith@pobox.com>
2013-08-22 17:22:17 -0400

    gofmt

--- zipcode_test.go
+++ zipcode_test.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2013 Public Good Software, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package zipcode
 
 import (
@@ -6,7 +26,7 @@ import (
 )
 
 func TestZIPCodeDB(t *testing.T) {
-	var testData = `"zip","city","state","latitude","longitude","timezone","dst"
+	var fixture = `"zip","city","state","latitude","longitude","timezone","dst"
 "21230","Baltimore","MD","39.273107","-76.62613","-5","1"
 "21231","Baltimore","MD","39.288607","-76.59116","-5","1"
 "21233","Baltimore","MD","39.284707","-76.620489","-5","1"
@@ -15,7 +35,7 @@ func TestZIPCodeDB(t *testing.T) {
 "21236","Nottingham","MD","39.389457","-76.48709","-5","1"
 "21237","Rosedale","MD","39.33224","-76.50365","-5","1"
 `
-	zdb, err := NewDB(bytes.NewReader([]byte(testData)))
+	zdb, err := NewDB(bytes.NewReader([]byte(fixture)))
 	if err != nil {
 		t.Fatalf("creating ZIP Code db: %v", err)
 	}
